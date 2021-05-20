bde145330363a5ee55ea07536efeba395e2d7e36
MAC <mac@MACs-MacBook-Pro.local>
2021-05-20 23:07:32 +0530

    initial commit

---
+++ VajinderSwiftChallenge/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  VajinderSwiftChallenge
+//
+//  Created by MAC on 17/05/21.
+//
+
+import Foundation
---
+++ VajinderSwiftChallenge/ViewModels/MainViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewModel.swift
+//  VajinderSwiftChallenge
+//
+//  Created by MAC on 17/05/21.
+//
+
+import Foundation
